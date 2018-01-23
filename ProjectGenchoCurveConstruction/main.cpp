786d6b4473cd87a3b6edfa6e703b9e24349c433c
yevhenii <yantonenko@optimumprime.nl>
2018-01-23 12:46:34 +0100

    test

--- ProjectGenchoCurveConstruction/main.cpp
+++ ProjectGenchoCurveConstruction/main.cpp
@@ -9,7 +9,7 @@ int main()
 	Cash obj(Currency("EUR"), 2, 3, 1, 0.01, 0.1);
 
 	auto cash = std::make_shared<Cash>(Currency("USD"), 2, 3, 1, 0.01, 0.1);
-	// hhgjghjg;
+	// hhgjghjg;hgh;
 	return 0;
 	//S s; // calls S::S()
 	//S s2(10); // calls S::S(int)
